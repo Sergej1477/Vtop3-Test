178e11c3bd9e50b0e10e9ef706ed1c5cac56c4ff
Sergej Burusas <Sergej_Burusas@mail.ru>
2023-09-04 22:55:09 +0300

    add files3

---
+++ src/components/promo/promo.js
@@ -0,0 +1,5 @@
+import './promo.scss';
+
+document.querySelector('.promo__link').onclick = function () {
+  window.location.href = 'https://actualtraffic.ru/site/megapartners/';
+};
--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ import './components/typography/typography.scss';
 import './components/container.scss';
 import './components/header/header';
 import './components/button/button';
+import './components/promo/promo';
+import './components/footer/footer';
+import './components/social-icons/social-icons';
+import './components/drawer/drawer';
